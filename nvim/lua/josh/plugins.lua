9ce4105212dbfd98427190947994b6f32a4c3a33
Joshua Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-01-23 15:38:38 -0800

    add bootstrapping packer

--- nvim/lua/josh/plugins.lua
+++ nvim/lua/josh/plugins.lua
@@ -1,3 +1,20 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path,
+  })
+  vim.notify('Installing packer close and reopen Neovim...')
+  vim.cmd([[packadd packer.nvim]])
+end
+
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -121,6 +138,9 @@ return require('packer').startup({
   function(use)
     for _, plugin in ipairs(plugins) do
       use(plugin)
+    end
+    if PACKER_BOOTSTRAP then
+      require('packer').sync()
     end
   end,
   config = {
