02db67eb90a7ccde5c1055e9717f749db7976a99
Joshua Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-04-11 19:30:30 -0700

    prepare for tresitter maintained deprecation

--- nvim/lua/josh/treesitter.lua
+++ nvim/lua/josh/treesitter.lua
@@ -1,5 +1,14 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = 'maintained',
+  ensure_installed = {
+    'c',
+    'css',
+    'dockerfile',
+    'javascript',
+    'json',
+    'lua',
+    'svelte',
+    'typescript',
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
