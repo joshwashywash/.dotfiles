2b1d3e4a7910fa6726adae2ec6176155ebcd1abe
Joshua Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-06-07 19:48:44 -0700

    add recommended workaround for multi offset encodings

--- nvim/lua/josh/null-ls.lua
+++ nvim/lua/josh/null-ls.lua
@@ -1,6 +1,14 @@
 local null_ls = require('null-ls')
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
+-- null-ls does not support multi offset_encodings. fix this if/when it does
+local notify = vim.notify
+vim.notify = function(msg, ...)
+  if not msg:match('warning: multiple different client offset_encodings') then
+    notify(msg, ...)
+  end
+end
+
 null_ls.setup({
   on_attach = function(client, bufnr)
     if client.supports_method('textDocument/formatting') then
