157be01647e6744559d0b989ac43e1bdab475816
Joshua Oertel <16182568+joshwashywash@users.noreply.github.com>
2022-06-11 16:19:14 -0700

    organize treesitter and actually figure it out

--- nvim/lua/josh/treesitter.lua
+++ nvim/lua/josh/treesitter.lua
@@ -18,9 +18,9 @@ require('nvim-treesitter.configs').setup({
     enable = true,
     keymaps = {
       init_selection = 'gnn',
+      node_decremental = 'grm',
       node_incremental = 'grn',
       scope_incremental = 'grc',
-      node_decremental = 'grm',
     },
   },
   indent = {
@@ -32,26 +32,26 @@ require('nvim-treesitter.configs').setup({
       lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
         ['ac'] = '@class.outer',
+        ['af'] = '@function.outer',
         ['ic'] = '@class.inner',
+        ['if'] = '@function.inner',
       },
     },
     move = {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
-        [']m'] = '@function.outer',
         [']]'] = '@class.outer',
+        [']m'] = '@function.outer',
       },
       goto_next_end = {
         [']M'] = '@function.outer',
         [']['] = '@class.outer',
       },
       goto_previous_start = {
-        ['[m'] = '@function.outer',
         ['[['] = '@class.outer',
+        ['[m'] = '@function.outer',
       },
       goto_previous_end = {
         ['[M'] = '@function.outer',
